392465df70031e4949aa213bd508c47dd37aede7
Nassim Afrete <nassim.afrete@gmail.com>
2025-12-29 19:46:48 +0100

    feat: integrate Prisma with PostgreSQL and set up initial database schema
    - Added Prisma dependencies to package.json.
    - Created prisma.config.ts for Prisma configuration.
    - Defined initial database schema in schema.prisma with User and Post models.
    - Added migration files for creating User and Post tables with necessary constraints.
    - Implemented database connection in db.ts using PrismaNeon adapter.
    - Updated page.tsx to fetch and display users from the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Page() {
+export default async function Page() {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="flex min-h-screen min-w-screen items-center justify-center">
       <Button size="lg" variant="default">
         hello world
+        {JSON.stringify(users)}
       </Button>
     </div>
   );
---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
